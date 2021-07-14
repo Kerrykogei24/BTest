422abb4ca70badb219db47628749e3a2b753a1df
Kerrykogei24 <kerrykomar@gmail.com>
2021-07-14 17:45:45 +0300

    made changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://fullstack-role.busara.io/api/v1/users/registration/',
-  apiAuth: 'http://fullstack-role.busara.io/api/v1/oauth/token/',
-  postUrl: 'http://fullstack-role.busara.io/api/v1/recruitment/answers/submit/',
-  userUrl: 'http://fullstack-role.busara.io/api/v1/users/current-user',
+  apiUrl: 'https://fullstack-role.busara.io/api/v1/users/registration/',
+  apiAuth: 'https://fullstack-role.busara.io/api/v1/oauth/token/',
+  postUrl: 'https://fullstack-role.busara.io/api/v1/recruitment/answers/submit/',
+  userUrl: 'https://fullstack-role.busara.io/api/v1/users/current-user',
   client_id: 'zVs3J7FZupB3TLPskQOy1xHLwYTRkzUSf2rdTDCu',
-  suervey: 'http://fullstack-role.busara.io/api/v1/recruitment/forms/',
+  suervey: 'https://fullstack-role.busara.io/api/v1/recruitment/forms/',
   secret_id: 'Zv19oWmm416sTyjWT5Sx2r1gRwjWrXU3P5dWledQpYjxEvavS58SPtz03M8wvsgajaVLhcimmJIUUYUDad06V6HQosmPoj3TPRNjg7bgniQlooIwyFWfz8KfkM5Tdh7R'
 
 };
