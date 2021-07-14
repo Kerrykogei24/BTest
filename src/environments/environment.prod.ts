29ef2c0b1e88eb2116b884dec1ff4d1939a75381
Kerrykogei24 <kerrykomar@gmail.com>
2021-07-14 17:15:15 +0300

    add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://fullstack-role.busara.io/api/v1/users/registration/',
+  apiAuth: 'http://fullstack-role.busara.io/api/v1/oauth/token/',
+  postUrl: 'http://fullstack-role.busara.io/api/v1/recruitment/answers/submit/',
+  userUrl: 'http://fullstack-role.busara.io/api/v1/users/current-user',
+  client_id: 'zVs3J7FZupB3TLPskQOy1xHLwYTRkzUSf2rdTDCu',
+  suervey: 'http://fullstack-role.busara.io/api/v1/recruitment/forms/',
+  secret_id: 'Zv19oWmm416sTyjWT5Sx2r1gRwjWrXU3P5dWledQpYjxEvavS58SPtz03M8wvsgajaVLhcimmJIUUYUDad06V6HQosmPoj3TPRNjg7bgniQlooIwyFWfz8KfkM5Tdh7R'
+
 };
